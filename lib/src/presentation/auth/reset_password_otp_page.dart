75246e851dd77b1003478c854801be2a2a3665b6
fahimalaminauntu <fahimalaminauntu@gmail.com>
2025-04-12 01:33:09 +0600

    problem 3

--- lib/src/presentation/auth/reset_password_otp_page.dart
+++ lib/src/presentation/auth/reset_password_otp_page.dart
@@ -7,8 +7,6 @@ import 'package:pinput/pinput.dart';
 import '../_common/widgets/app_button.dart';
 import 'auth.dart';
 
-class OtpPage extends StatelessWidget {
-  const OtpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
