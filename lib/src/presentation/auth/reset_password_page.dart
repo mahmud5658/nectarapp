aaf57ac01b36b72b7d0742f6895b329122e2488e
fahimalaminauntu <fahimalaminauntu@gmail.com>
2025-04-12 01:34:53 +0600

    error

--- lib/src/presentation/auth/reset_password_page.dart
+++ lib/src/presentation/auth/reset_password_page.dart
@@ -7,20 +7,7 @@ import 'auth.dart';
 class ResetPasswordPage extends StatelessWidget {
   const ResetPasswordPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    const gap = SizedBox(height: 24);
-    final textTheme = Theme.of(context).textTheme;
-    return AuthBackGroundWrapper(
-      title: "Verification",
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            "Enter your email here ! we will send you verification code.",
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 32),
+ 
           const AppTextField(
             hint: "Email",
             icon: Icon(Icons.email),
